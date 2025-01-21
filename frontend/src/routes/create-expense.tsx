b92acbc10500542260926ac5a39a979e7b96d8e0
toho36 <toho0036@gmail.com>
2025-01-21 18:08:54 +0100

    create expense
    static visual

--- frontend/src/routes/create-expense.tsx
+++ frontend/src/routes/create-expense.tsx
@@ -1,3 +1,6 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/create-expense')({
@@ -5,5 +8,18 @@ export const Route = createFileRoute('/create-expense')({
 });
 
 function CreateExpense() {
-  return <div>Create Expense</div>;
+  return (
+    <div className="p-2">
+      <h2>Create Expense</h2>
+      <form className="max-w-xl m-auto">
+        <Label htmlFor="title">Title</Label>
+        <Input type="text" id="title" placeholder="Title" />
+        <Label htmlFor="amount">Amount</Label>
+        <Input type="number" id="amount" placeholder="Amount" />
+        <Button className="mt-4" type="submit">
+          Create Expense
+        </Button>
+      </form>
+    </div>
+  );
 }
