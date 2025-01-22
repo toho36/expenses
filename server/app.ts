29ebbebff0fcece980f36ccfe30e9360aea82367
toho36 <toho0036@gmail.com>
2025-01-22 12:26:40 +0100

    basic kinde auth
    endpoints for login and register
    session manager to store details in memory

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,7 @@ import { logger } from 'hono/logger';
 import { expensesRoute } from './routes/expenses';
 import { handle } from 'hono/vercel';
 import { serveStatic } from 'hono/bun';
+import { authRoute } from './routes/auth';
 
 export const runtime = 'edge';
 
@@ -10,7 +11,10 @@ const app = new Hono();
 
 app.use('*', logger());
 
-const ApiRoutes = app.basePath('/api').route('expenses', expensesRoute);
+const ApiRoutes = app
+  .basePath('/api')
+  .route('expenses', expensesRoute)
+  .route('/authRoute', authRoute);
 
 app.get('*', serveStatic({ root: './frontend/dist' }));
 app.get('*', serveStatic({ path: './frontend/dist/index.html' }));
